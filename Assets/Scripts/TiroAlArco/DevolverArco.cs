4cad4ca602500c6ea8649abb28c6954ca67c167f
smallPingu <arturomolina@correo.ugr.es>
2025-09-14 12:07:09 +0200

    Empezamos preparando monedas para RotacionCoches. Aplicamos shader de efecto curvo para materiales nuevos de la carretera

--- Assets/Scripts/TiroAlArco/DevolverArco.cs
+++ Assets/Scripts/TiroAlArco/DevolverArco.cs
@@ -12,7 +12,8 @@ public class DevolverArco : MonoBehaviour
     public Transform objetoARecordar;
 
     private TransformData datosIniciales;
-    AudioSource sonido;
+    public AudioSource sonido;
+    public GameObject prefabParticulasReset;
 
     private bool isInicializado = false;
 
@@ -35,6 +36,21 @@ public class DevolverArco : MonoBehaviour
             objetoARecordar.position = datosIniciales.posicion;
             objetoARecordar.rotation = datosIniciales.rotacion;
             objetoARecordar.localScale = datosIniciales.escala;
+
+            if (prefabParticulasReset != null)
+            {
+                GameObject efectoInstanciado = Instantiate(prefabParticulasReset, datosIniciales.posicion, Quaternion.identity);
+
+                ParticleSystem ps = efectoInstanciado.GetComponent<ParticleSystem>();
+                if (ps != null)
+                {
+                    Destroy(efectoInstanciado, ps.main.duration + ps.main.startLifetime.constantMax);
+                }
+                else
+                {
+                    Destroy(efectoInstanciado, 3f); 
+                }
+            }
         }
     }
 
